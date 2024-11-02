2cd35343a07afa0a3952d9a6c38a715ab4f7709f
jikei334 <ikimono_lover@yahoo.co.jp>
2024-11-02 18:34:10 +0900

    add deol

--- nvim/lua/mapping.lua
+++ nvim/lua/mapping.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = ','
 
 -- terminal
-vim.keymap.set('n', '<leader>sh', '<cmd>terminal<cr>')
-vim.keymap.set('t', '<C-[>', '<C-\\><C-n>')
+-- vim.keymap.set('n', '<leader>sh', '<cmd>terminal<cr>')
+-- vim.keymap.set('t', '<C-[>', '<C-\\><C-n>')
